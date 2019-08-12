dbfb554f1f5aea698abc8800d3572987f70d0f06
Esin Ozcan <esinozcan@sabanciuniv.edu>
2019-08-12 20:51:50 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'games/new'
+  get 'games/score'
   get 'questions/ask'
   get 'questions/answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
